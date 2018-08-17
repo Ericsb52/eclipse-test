7b4b88b16718d206e43e51eec1fcb1c32832176b
Ericsb52 <Broadbent52@aol.com>
2018-08-17 09:18:54 -0600

    first commit

---
+++ EricTest/src/Eric/Test/EricTest.java
@@ -0,0 +1,14 @@
+package Eric.Test;
+
+public class EricTest {
+
+	public EricTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello World");
+
+	}
+
+}
